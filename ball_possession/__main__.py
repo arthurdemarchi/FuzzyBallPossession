815f4e75dceb7fb18ebdc361d6ecd73da27927e2
arthurdemarchi <hdemarchi@localhost.localdomain>
2019-12-08 23:12:14 -0300

    rules added

--- ball_possession/__main__.py
+++ ball_possession/__main__.py
@@ -5,7 +5,12 @@ from .fuzzy.region import Region
 from .graphics.plot_region import plot_region
 import pandas as pd
 
-## CONSTANTS
+#########################################################
+#                                                       #
+#                      COSNTANTS                        #
+#                                                       #
+#########################################################
+
 PS = 0.3 # PLAYER_SIZE
 PB = 0.085 # PLAYER_BODY
 CPD = PS+PB # CERTAIN POSSESSION DISTANCE
@@ -15,9 +20,11 @@ DD = 1.2*2*1.05 # DASHABLE DISTANCE
 PMS = 1.2*1.05 # PLAYER MAXIMUM SPEED
 RS = (2*KA)/1.5 # REACTION SPEED
 
-#create reader with default data path values
-reader = Reader()
-
+#########################################################
+#                                                       #
+#                        MODEL                          #
+#                                                       #
+#########################################################
 #create fuzzy input regions
 ## input 1: distance from player to ball
 dist_minor = Region('D1', 1000, 0, 100, Trapezoid(0, 1.5*CPD, 0, CPD).function)
@@ -41,8 +48,30 @@ bposs_false = Region('BPF', 10, 0, 1, Triangular(0, 0.75).function)
 bposs_true = Region('BPT', 10, 0, 1, Triangular(0.5, 1).function)
 ball_possession = [bposs_false, bposs_true]
 
+#########################################################
+#                                                       #
+#                        RULES                          #
+#                                                       #
+#########################################################
+def rules(dist, rspeed, bspeed):
+    if dist == dist_minor and rspeed == rspeed_minor and bspeed == bspeed_minor: return bposs_true
+    if dist == dist_minor and rspeed == rspeed_minor and bspeed == bspeed_great: return bposs_true
+    if dist == dist_minor and rspeed == rspeed_great and bspeed == bspeed_minor: return bposs_true
+    if dist == dist_minor and rspeed == rspeed_great and bspeed == bspeed_great: return bposs_true
+    if dist == dist_fair  and rspeed == rspeed_minor and bspeed == bspeed_minor: return bposs_true
+    if dist == dist_fair  and rspeed == rspeed_minor and bspeed == bspeed_great: return bposs_true
+    if dist == dist_fair  and rspeed == rspeed_great and bspeed == bspeed_minor: return bposs_true
+    if dist == dist_fair  and rspeed == rspeed_great and bspeed == bspeed_great: return bposs_false
+    if dist == dist_great and rspeed == rspeed_minor and bspeed == bspeed_minor: return bposs_false
+    if dist == dist_great and rspeed == rspeed_minor and bspeed == bspeed_great: return bposs_false
+    if dist == dist_great and rspeed == rspeed_great and bspeed == bspeed_minor: return bposs_false
+    if dist == dist_great and rspeed == rspeed_great and bspeed == bspeed_great: return bposs_false
+
+
+#create reader with default data path values
+reader = Reader()
+
 #while there's a file to read run application
 status = True
 while status:
     status, distance, relative_speed, ball_speed = reader.read_next_file()
-    print(ball_speed.max())
