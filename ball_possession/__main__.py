4caaa1fbcbd3cff72973acc9daa357c93ff0370b
arthurdemarchi <hdemarchi@localhost.localdomain>
2019-12-08 22:50:10 -0300

    ball absolute speed and output instanciated in main

--- ball_possession/__main__.py
+++ ball_possession/__main__.py
@@ -20,31 +20,29 @@ reader = Reader()
 
 #create fuzzy input regions
 ## input 1: distance from player to ball
-dist_minor = Region('D3', 1000, 0, 100, Trapezoid(0, 1.5*CPD, 0, CPD).function)
+dist_minor = Region('D1', 1000, 0, 100, Trapezoid(0, 1.5*CPD, 0, CPD).function)
 dist_fair = Region('D2', 1000, 0, 100, Triangular(CPD, 1.5*KA, KA).function)
-dist_great = Region('D1', 1000, 0, 100, Trapezoid(KA, 100, DD, 100).function)
+dist_great = Region('D3', 1000, 0, 100, Trapezoid(KA, 100, DD, 100).function)
 distance = [dist_minor, dist_fair, dist_great]
 
 ## input 2: relative speed from ball to player
-rspeed_minor = Region('RS3', 50, 0, 5, Trapezoid(0, 1.5*RS, 0, RS).function)
-rspeed_great = Region('RS1', 50, 0, 5, Trapezoid(RS, 5, 1.5*RS, 5).function)
+rspeed_minor = Region('RS1', 50, 0, 5, Trapezoid(0, 1.5*RS, 0, RS).function)
+rspeed_great = Region('RS2', 50, 0, 5, Trapezoid(RS, 5, 1.5*RS, 5).function)
 relative_speed = [rspeed_minor, rspeed_great]
 
 ## input 3: ball absolute speed
-bspeed_minor = Region('BS3', 100, 0, 30, Triangular(10, 20, 15, 1, 0).function)
-bspeed_fair = Region('BS2', 100, 0, 30, Triangular(10, 20, 15, 1, 0).function)
-bspeed_great = Region('BS1', 100, 0, 30, Triangular(10, 20, 15, 1, 0).function)
-ball_speed = [bspeed_minor, bspeed_fair, bspeed_minor]
+bspeed_minor = Region('BS1', 100, 0, 10, Trapezoid(0, 1.5*PMS, 0, PMS).function)
+bspeed_great = Region('BS2', 100, 0, 10, Trapezoid(PMS, 10, PMS, 10).function)
+ball_speed = [bspeed_minor, bspeed_minor]
 
 # output
 ## ball possession
-bposs_minor = Region('BP3', 100, 0, 30, Triangular(10, 20, 15, 1, 0).function)
-bposs_fair = Region('BP2', 100, 0, 30, Triangular(10, 20, 15, 1, 0).function)
-bposs_great = Region('BP1', 100, 0, 30, Triangular(10, 20, 15, 1, 0).function)
-ball_possession = [bposs_minor, bposs_fair, bposs_great]
+bposs_false = Region('BPF', 10, 0, 1, Triangular(0, 0.75).function)
+bposs_true = Region('BPT', 10, 0, 1, Triangular(0.5, 1).function)
+ball_possession = [bposs_false, bposs_true]
 
 #while there's a file to read run application
 status = True
 while status:
     status, distance, relative_speed, ball_speed = reader.read_next_file()
-    print(relative_speed.max())
+    print(ball_speed.max())
